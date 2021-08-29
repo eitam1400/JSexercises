78391faa03f0737bc6ff6d2ec691478117c2527d
eitam1400 <eitamshalom@gmail.com>
2021-08-30 01:01:23 +0300

    #2 excercise
    quadratic equation solution

--- intro_js.js
+++ intro_js.js
@@ -1,8 +1,19 @@
+\\first exercise
 const num = 123;
 let sum = 0;
 let n1=Math.floor(num%10);
 let n2=Math.floor((num/10)%10);
 let n3= Math.floor(num/100);
 sum = n1+n2+n3;
-// your code goes here;
 console.log(sum);
+
+
+\\second exercise
+const a = 1; b = -6; c = 9;  // stands for x**2 - 6*x + 9 = 0
+let solutionA = solutionB = 0;
+solutionA = ((-b + Math.sqrt((Math.pow(b,2))-(4ac))) / (2a));
+solutionB = ((-b - Math.sqrt((Math.pow(b,2))-(4ac))) / (2a));
+console.log(solutionA, solutionB)
+
+
+
