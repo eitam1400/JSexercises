d0f7c9c49141ad35d638c76ec13c8219babc7534
eitam1400 <eitamshalom@gmail.com>
2021-08-31 23:21:36 +0300

    return count of winning F1 drivers
    initialises their counters in the object and increases it everytime it runs on someone' name from the array(list).
    
    Co-Authored-By: cyber4s <46290715+cyber4s@users.noreply.github.com>

--- F1WinnersBabyyy.js
+++ F1WinnersBabyyy.js
@@ -23,62 +23,18 @@ const formula1Champions = [
     "Hamilton"
 ];
 
-function countW(driver,lst)
-{
-    let hamiltonArr=[];
-    let rosbergArr=[];
-    let vettelArr=[];
-    let alonsoArr=[];
-    let räikkönenArr=[];
-    let buttonArr=[];
-    let schumacherArr=[];
-
-    for(let i=0; i<lst.length;i++)
-    {
-        hamiltonArr = lst.filter(lst[i] === "Hamilton"); // Creating array that restore all the winnings of the specific driver onlyin on place
-        rosbergArr=lst.filter(lst[i] === "Rosberg");
-        vettelArr=lst.filter(lst[i] === "Vettel");
-        alonsoArr=lst.filter(lst[i] === "Alonso");
-        räikkönenArr=lst.filter(lst[i] === "Räikkönen");
-        buttonArr=lst.filter(lst[i] === "Button");
-        schumacherArr=lst.filter(lst[i] === "Schumacher");
-    }
-
-     switch(driver){ //checking the guy wins and returning it
-         case "Hamilton":
-             return hamiltonArr.length;
-         case "Rosberg":
-             return rosbergArr.length;
-         case "Vettel":
-             return rosbergArr.length;
-         case "Alonso":
-             return  alonsoArr.length;  
-         case "Räikkönen":
-             return räikkönenArr.length;
-         case "Button":
-             return  buttonArr.length;
-         case "Schumacher":
-             return  schumacherArr.length;
-     }
-}
-
 let winners = {};
 
-//let winCounter = countW(driver,formula1Champions);
-
-winners["Hamilton"] =countW("Hamilton",formula1Champions);
-winners["Schumacher"] =countW("Schumacher",formula1Champions);
-winners["Räikkönen"] =countW("Räikkönen",formula1Champions);
-winners["Alonso"] =countW("Alonso",formula1Champions);
-winners["Vettel"] =countW("Vettel",formula1Champions);
-winners["Rosberg"] =countW("Rosberg",formula1Champions);
-winners["Button"] =countW("Button",formula1Champions);
-
-console.log(winners);
-
-
 function countWin(driver) {
-    
-}
+    if (!winners[driver])
+         winners[driver] = 0
+    winners[driver]++
+  }
 
-formula1Champions.forEach(countWin)
+
+formula1Champions.forEach(countWin);
+
+
+
+
+
